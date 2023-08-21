16f8e3965431c7d402f3952a8e5db42a815d2e75
Mosse-Sox <autumngreenie990@gmail.com>
2023-08-21 23:30:55 +0000

    completes exercise 1

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -4,3 +4,34 @@ puts "Exercise 1"
 puts "----------"
 
 # Your code goes below here ...
+
+class Store < ActiveRecord::Base
+  include ActiveModel::Validations
+
+  validates :name, presence: true
+  validates :annual_revenue, numericality: { greater_than_or_equal_to: 0 }
+end
+
+Store.create(
+  name: 'Burnaby',
+  annual_revenue: 300000,
+  mens_apparel: true,
+  womens_apparel: true
+)
+
+Store.create(
+  name: 'Richmond',
+  annual_revenue: 1260000,
+  mens_apparel: false,
+  womens_apparel: true
+)
+
+Store.create(
+  name: 'Gastown',
+  annual_revenue: 190000,
+  mens_apparel: true,
+  womens_apparel: false
+)
+
+puts "Number of stores in the database: #{Store.count}"
+
