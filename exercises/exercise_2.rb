2ca1767c40a0426c6a5924823bc1d60bfe19d7ac
Mosse-Sox <autumngreenie990@gmail.com>
2023-08-21 23:37:27 +0000

    completes exercise 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,10 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+@store1 = Store.find(1)
+@store2 = Store.find(2)
+
+puts @store1.name
+@store1.name = "Beef Burrito"
+puts @store1.name
